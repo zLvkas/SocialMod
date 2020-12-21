ae9ca9254cec636faf27171bfb6418d9a22d3b93
Lukas <buddilp90@gmail.com>
2020-12-21 20:38:19 +0100

    Added ban message

--- src/main/java/de/socialmod/addon/server/response/UpdateResponse.java
+++ src/main/java/de/socialmod/addon/server/response/UpdateResponse.java
@@ -10,33 +10,38 @@ public enum UpdateResponse {
             200
     ),
 
+    FORBIDDEN(
+            Constants.PREFIX + "§7Du wurdest dauerhaft §cgesperrt§7!",
+            401
+    ),
+
     TOO_MANY_REQUESTS(
-            Constants.PREFIX + "§7Du kannst deine §eSocialMedias §7nur alle §610 Sekunden §7updaten!",
+            Constants.PREFIX +"§7Du kannst deine §eSocialMedias §7nur alle §610 Sekunden §7updaten!",
             429
     ),
 
     NAME_TOO_LONG(
-            Constants.PREFIX + "§7Dein %media% Name §7ist §czu lang§7! §8(§7maximal 32 Zeichen§8)",
+            Constants.PREFIX +"§7Dein %media% Name §7ist §czu lang§7! §8(§7maximal 32 Zeichen§8)",
             418
     ),
 
     UNKNOWN_ERROR(
-            Constants.PREFIX + "§7Ein §cunbekannter Fehler §7ist aufgetreten!",
+            Constants.PREFIX +"§7Ein §cunbekannter Fehler §7ist aufgetreten!",
             -1
     ),
 
     INTERNAL_SERVER_ERROR(
-            Constants.PREFIX + "§7Derzeit ist unser Server leider §cnicht §7erreichbar. Versuche es bitte später erneut!",
+            Constants.PREFIX +"§7Derzeit ist unser Server leider §cnicht §7erreichbar. Versuche es bitte später erneut!",
             500
     ),
 
     AUTH_FAILED(
-            Constants.PREFIX + "§7Authentifizierung §cfehlgeschlagen§7! Bitte starte dein Spiel neu.",
+            Constants.PREFIX +"§7Authentifizierung §cfehlgeschlagen§7! Bitte starte dein Spiel neu.",
             403
     ),
 
     BAD_REQUEST(
-            Constants.PREFIX + "§7Deine Anfrage konnte §cnicht §7richtig verarbeitet werden!",
+            Constants.PREFIX +"§7Deine Anfrage konnte §cnicht §7richtig verarbeitet werden!",
             400
     );
 
